98c2a32686e029b8f5fdf9c48d3157816b720d82
andrewpetrenko1 <andreypetrenko1998@gmail.com>
2020-05-15 15:55:44 +0300

    update console to gem 0.1.3, added language selection

--- console.rb
+++ console.rb
@@ -15,10 +15,31 @@ class Console
       check_options(option)
       break if @exit == true
     end
-    puts I18n.t(:goodbye, name: @player.name)
+    show_goodbye_message
   end
 
   private
+
+  def show_welcome_message
+    puts I18n.t(:choose_option)
+    puts "#{I18n.t(:start_option)} \n#{I18n.t(:rules_option)}"
+    puts "#{I18n.t(:stats_option)} \n#{I18n.t(:exit_option)}"
+  end
+
+  def show_goodbye_message
+    puts I18n.t(:goodbye, name: @player.name) unless @player.name.nil?
+    puts I18n.t(:goodbye_unknown) if @player.name.nil?
+  end
+
+  def check_options(option)
+    case option
+    when I18n.t(:start) then start_game
+    when I18n.t(:rules) then puts @game.show_rules
+    when I18n.t(:stats) then @game.show_stats
+    when I18n.t(:exit) then close_game
+    else puts I18n.t(:option_error)
+    end
+  end
 
   def start_game
     set_player_name
@@ -29,6 +50,55 @@ class Console
     end
     save_stat? if @game.win?
     try_again? if @exit != true
+  end
+
+  def set_player_name
+    loop do
+      puts I18n.t(:enter_name)
+      player_name = STDIN.gets.chomp.capitalize
+      break if (errors = @player.setup_name(player_name)) == player_name
+
+      puts errors
+    end
+    puts I18n.t(:hello, name: @player.name)
+  end
+
+  def set_difficulty
+    loop do
+      puts I18n.t(:enter_difficulty)
+      puts CodebreakerAp::Difficulty::DIFFICULTY.keys
+      difficulty_level = STDIN.gets.chomp
+      errors = @game.difficulty.initialize_difficulty(difficulty_level)
+      break unless @game.difficulty.attempts.nil?
+
+      puts errors
+    end
+  end
+
+  def game_process
+    show_game_options
+    puts @game.secret_code.join
+    enter_game_option
+    puts @game.check_answer(@player.answer)
+  end
+
+  def show_game_options
+    puts I18n.t(:show_hints_and_attempts, hints: @game.difficulty.hints, attempts: @game.difficulty.attempts)
+    puts "#{I18n.t(:guess_secret_code)}\n#{I18n.t(:take_hint)}\n#{I18n.t(:exit_option)}"
+  end
+
+  def enter_game_option
+    loop do
+      case answer = STDIN.gets.chomp
+      when HINT then puts I18n.t(:hint, hint: @game.take_hint)
+      when I18n.t(:exit) then close_game && break
+      else
+        errors = @player.setup_answer(answer)
+        break unless @player.answer.nil?
+
+        puts errors
+      end
+    end
   end
 
   def save_stat?
@@ -42,69 +112,6 @@ class Console
     again = STDIN.gets.chomp.downcase
     close_game unless again == YES
     @game = CodebreakerAp::Game.new
-  end
-
-  def game_process
-    show_game_options
-    puts @game.secret_code.join
-    enter_game_option
-    puts @game.check_answer(@player.answer) if @player.validated
-  end
-
-  def enter_game_option
-    @player.validated = false
-    answer = STDIN.gets.chomp
-    case answer
-    when HINT then puts I18n.t(:hint, hint: @game.take_hint)
-    when I18n.t(:exit) then close_game
-    else @player.setup_answer(answer)
-    end
-  end
-
-  def show_welcome_message
-    puts I18n.t(:choose_option)
-    puts I18n.t(:start_option)
-    puts I18n.t(:rules_option)
-    puts I18n.t(:stats_option)
-    puts I18n.t(:exit_option)
-  end
-
-  def show_game_options
-    puts I18n.t(:show_hints_and_attempts, hints: @game.difficulty.hints, attempts: @game.difficulty.attempts)
-    puts "#{I18n.t(:guess_secret_code)}\n#{I18n.t(:take_hint)}\n#{I18n.t(:exit_option)}"
-  end
-
-  def check_options(option)
-    case option
-    when I18n.t(:start) then start_game
-    when I18n.t(:rules) then @game.show_rules
-    when I18n.t(:stats) then @game.show_stats
-    when I18n.t(:exit) then close_game
-    else
-      puts I18n.t(:option_error)
-    end
-  end
-
-  def set_player_name
-    loop do
-      puts I18n.t(:enter_name)
-      player_name = STDIN.gets.chomp.capitalize
-      @player.setup_name(player_name)
-      break if @player.validated
-    end
-    puts I18n.t(:hello, name: @player.name)
-  end
-
-  def set_difficulty
-    loop do
-      puts I18n.t(:enter_difficulty)
-      puts CodebreakerAp::Difficulty::DIFFICULTY.keys
-      difficulty_level = STDIN.gets.chomp
-      @game.difficulty.initialize_difficulty(difficulty_level)
-      break if @game.difficulty.validated
-
-      puts @game.difficulty.errors
-    end
   end
 
   def close_game
--- main.rb
+++ main.rb
@@ -1,5 +1,20 @@
 require_relative 'autoload.rb'
+
 I18n.load_path << Dir[File.expand_path('locales') + '/*.yml']
-I18n.default_locale = :en
+
+loop do
+  puts "Choose language. \nType `en` to select English, `ru` to select Russian."
+  language = STDIN.gets
+  case language
+  when "ru\n"
+    I18n.default_locale = :ru
+    break
+  when "en\n"
+    I18n.default_locale = :en
+    break
+  else
+    puts 'Wrong locale!'
+  end
+end
 
 Console.new.start
